0c6fde96f218adf6977a02509a5a2ee97f4f22cf
tanabe-sunao <tanabe.sunao@dena.jp>
2014-07-06 14:18:55 +0900

    recipe の生成を閉じ込める

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -4,13 +4,19 @@ require 'recipe_source'
 require 'recipe_viewer'
 
 class Recipe
+  DEFAULT_PATH = 'data/recipes.yaml'
+
   attr_accessor :title
   def initialize recipe
     @title = recipe[:title]
   end
+
+  def self.all path = DEFAULT_PATH
+    recipes = RecipeSource.new(path).read
+    recipes.map {|recipe| new(recipe) }
+  end
 end
 
 if __FILE__ == $0
-  recipes = RecipeSource.new('data/recipes.yaml').read
-  RecipeViewer.new.show_recipes recipes.map {|recipe| Recipe.new(recipe) }
+  RecipeViewer.new.show_recipes Recipe.all
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -2,11 +2,14 @@ require 'test/unit'
 require_relative '../lib/recipe'
 
 class RecipeTest < Test::Unit::TestCase
-  def setup
-    @recipe = Recipe.new(title: '握り飯')
+  def test_title
+    recipe = Recipe.new(title: '握り飯')
+    assert_equal '握り飯', recipe.title
   end
 
-  def test_recipe
-    assert_equal '握り飯', @recipe.title
+  def test_all
+    recipes = Recipe.all('test/recipe_sample.yaml')
+    assert_equal 2, recipes.count
+    assert_equal '麦飯', recipes.last.title
   end
 end
